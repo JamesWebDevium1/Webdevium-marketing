ef6f5f6491b2c7fd841d45816af7063fd7b53d78
James <james@webdevium.com>
2025-10-14 08:21:01 -0400

    fix

--- components/Prose.tsx
+++ components/Prose.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from 'react';
 import { cn } from '@/lib/utils';
 
 interface ProseProps {
-  children: ReactNode;
+  children?: ReactNode;
   className?: string;
   html?: string;
 }
